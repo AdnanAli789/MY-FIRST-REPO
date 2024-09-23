a1b56c271bfded71b18aa0acd9bf92004896f353
AdnanAli789 <adnanalishar93@gmail.com>
2024-09-23 00:28:14 -0700

    adding files and assignment
    for workshop

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+	cout<<"Subject\t\tMarks " <<endl;
+	cout<<"Maths\t\t95 " <<endl;
+	cout<<"English\t\t87 " <<endl;
+	cout<<"Programming\t90 " <<endl;
+	cout<<"Chemistry\t85 " <<endl;
+	return 0;
+	
+}
